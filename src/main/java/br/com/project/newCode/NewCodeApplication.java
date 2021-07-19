7da02794c8fd3ae128be0f5ae6539cf4370160d7
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-07-19 10:21:10 -0300

    Inicialização do projeto com Spring initializr

---
+++ src/main/java/br/com/project/newCode/NewCodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.newCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/project/newCode/NewCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.newCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
